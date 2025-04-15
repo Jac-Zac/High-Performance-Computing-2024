1d6189c9f23f11488bb4f38e9002a735c39dd9f4
luca <luca.tornatore@inaf.it>
2025-04-15 19:02:25 +0200

    fixing bnch of typos and the cast to double error in the final printf

--- lecture13-OMP--parallel-regions/parallel_regions/pi_omp.c
+++ lecture13-OMP--parallel-regions/parallel_regions/pi_omp.c
@@ -1,9 +1,7 @@
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#ìnclude <omp.h>
+#include <omp.h>
 
 
 #define CPU_TIME_W ({ struct timespec ts; (clock_gettime( CLOCK_REALTIME, &ts ), \
@@ -22,15 +20,18 @@ int main ( int argc, char **argv )
 
   uint Nshots       = (argc>1 ? (uint)atoi(*(argv+1)) : 1000000 );
   uint valid_points = 0;
-  
-  srand48(time(NULL));
+  int  nthreads;
+
   double timing = CPU_TIME_P;
 
   
   #pragma omp parallel
   {
+   #pragma omp masked
+    nthreads = omp_get_num_threads();
+    
     int                myid    = omp_get_thread_num();
-    unsigned short int seed[3] = { myid, myid & 123, myid*11 };
+    unsigned short int seed[3] = { (short int)(time(NULL))+myid, myid & 123, myid*11 };
     uint               my_validpoints = 0;
 
     double mytiming = CPU_TIME_T;
@@ -38,22 +39,25 @@ int main ( int argc, char **argv )
       {
 	double x = erand48(seed);
 	double y = erand48(seed);
+
+	my_validpoints += ( (x*x + y*y) <= 1 );
 	
-	// 2) check whether they are in the circle of radius 1
-	// 3) increase the local counter my_validpoints
       }
     mytiming = CPU_TIME_T - mytiming;
 
     // 4) get the total counter valid_points
+
+   #pragma omp atomic update
+    valid_points += my_validpoints;
     
-    printf("thread %d has run for %g sec\n", mytiming );
+    printf("thread %d has run for %g sec\n", myid, mytiming );
   }
   
   timing = CPU_TIME_P - timing;
     
   printf("value of pi greek is: %g\n"
 	 "elapsed time is %g sec\n",
-	 valid_points/Nshots * 4, timing);
+	 (double)valid_points/(nthreads*Nshots) * 4, timing);
   
 
   return 0;
