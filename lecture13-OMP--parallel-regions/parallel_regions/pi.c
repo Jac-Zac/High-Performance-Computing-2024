d655b293541c133a47ad121abd873a174b9e9eaa
Jac-Zac <jacopozac@icloud.com>
2025-08-05 20:16:21 +0200

    Faster implementation

--- lecture13-OMP--parallel-regions/parallel_regions/pi.c
+++ lecture13-OMP--parallel-regions/parallel_regions/pi.c
@@ -1,40 +1,40 @@
-
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-
-#define CPU_TIME_P ({ struct timespec myts; (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &myts ), \
-                                             (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9); })
-
+// Macro for timing
+#define CPU_TIME_P                                                             \
+  ({                                                                           \
+    struct timespec myts;                                                      \
+    (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &myts),                           \
+     (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9);                       \
+  })
 
 typedef unsigned int uint;
 
+int main(int argc, char **argv) {
 
-int main ( int argc, char **argv )
-{
-
-  uint Nshots       = (argc>1 ? (uint)atoi(*(argv+1)) : 1000000 );
+  uint Nshots = (argc > 1 ? (uint)atoi(*(argv + 1)) : 1000000);
   uint valid_points = 0;
-  
+
+  // define the random number generator with a seed given by the time
   srand48(time(NULL));
 
   double timing = CPU_TIME_P;
-  for ( uint i = 0; i < Nshots; i++ )
-    {
-      // 1) generate x and y by rand48()
-      // 2) check whether they are in the circle of radius 1
-      // 3) increase the counter
-      double x = drand48();
-      double y = drand48();
-      valid_points += (x*x + y*y <= 1);
-    }
+  for (uint i = 0; i < Nshots; i++) {
+    // 1) generate x and y by rand48()
+    double x = drand48();
+    double y = drand48();
+    // 2) check whether they are in the circle of radius 1
+    // 3) increase the counter
+    // The two steps are done in a branch less way
+    valid_points += (x * x + y * y <= 1);
+  }
   timing = CPU_TIME_P - timing;
-  
+
   printf("value of pi greek is: %g\n"
-	 "elapsed time is %g sec\n",
-	 (double)valid_points/Nshots * 4, timing);
+         "elapsed time is %g sec\n",
+         (double)valid_points / Nshots * 4, timing);
 
   return 0;
 }
--- lecture13-OMP--parallel-regions/parallel_regions/pi_omp.c
+++ lecture13-OMP--parallel-regions/parallel_regions/pi_omp.c
@@ -1,64 +1,75 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-
-#define CPU_TIME_W ({ struct timespec ts; (clock_gettime( CLOCK_REALTIME, &ts ), \
-                                           (double)ts.tv_sec + (double)ts.tv_nsec * 1e-9); })
-#define CPU_TIME_T ({ struct timespec myts; (clock_gettime( CLOCK_THREAD_CPUTIME_ID, &myts ), \
-                                             (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9); })
-#define CPU_TIME_P ({ struct timespec myts; (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &myts ), \
-                                             (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9); })
-
+// Define different timers
+#define CPU_TIME_W                                                             \
+  ({                                                                           \
+    struct timespec ts;                                                        \
+    (clock_gettime(CLOCK_REALTIME, &ts),                                       \
+     (double)ts.tv_sec + (double)ts.tv_nsec * 1e-9);                           \
+  })
+#define CPU_TIME_T                                                             \
+  ({                                                                           \
+    struct timespec myts;                                                      \
+    (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &myts),                            \
+     (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9);                       \
+  })
+#define CPU_TIME_P                                                             \
+  ({                                                                           \
+    struct timespec myts;                                                      \
+    (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &myts),                           \
+     (double)myts.tv_sec + (double)myts.tv_nsec * 1e-9);                       \
+  })
 
 typedef unsigned int uint;
 
+int main(int argc, char **argv) {
 
-int main ( int argc, char **argv )
-{
-
-  uint Nshots       = (argc>1 ? (uint)atoi(*(argv+1)) : 1000000 );
+  // Get argument from command line
+  uint Nshots = (argc > 1 ? (uint)atoi(*(argv + 1)) : 1000000);
   uint valid_points = 0;
-  int  nthreads;
+  int nthreads;
 
   double timing = CPU_TIME_P;
 
-  
-  #pragma omp parallel
+#pragma omp parallel
   {
-   #pragma omp masked
+    // This block is executed by only one thread (the first that encounters it)
+#pragma omp masked
     nthreads = omp_get_num_threads();
-    
-    int                myid    = omp_get_thread_num();
-    unsigned short int seed[3] = { (short int)(time(NULL))+myid, myid & 123, myid*11 };
-    uint               my_validpoints = 0;
+
+    int myid = omp_get_thread_num();
+    // Get different seeds for each thread
+    unsigned short int seed[3] = {(short int)(time(NULL)) + myid, myid & 123,
+                                  myid * 11};
+    uint my_validpoints = 0;
 
     double mytiming = CPU_TIME_T;
-    for ( uint i = 0; i < Nshots; i++ )
-      {
-	double x = erand48(seed);
-	double y = erand48(seed);
+// The pragma for allows for better distribution
+#pragma omp for
+    for (uint i = 0; i < Nshots; i++) {
+      double x = erand48(seed);
+      double y = erand48(seed);
 
-	my_validpoints += ( (x*x + y*y) <= 1 );
-	
-      }
+      my_validpoints += ((x * x + y * y) <= 1);
+    }
     mytiming = CPU_TIME_T - mytiming;
 
     // 4) get the total counter valid_points
 
-   #pragma omp atomic update
+#pragma omp atomic update
     valid_points += my_validpoints;
-    
-    printf("thread %d has run for %g sec\n", myid, mytiming );
+
+    printf("thread %d has run for %g sec\n", myid, mytiming);
   }
-  
+
   timing = CPU_TIME_P - timing;
-    
+
   printf("value of pi greek is: %g\n"
-	 "elapsed time is %g sec\n",
-	 (double)valid_points/(nthreads*Nshots) * 4, timing);
-  
+         "elapsed time is %g sec\n",
+         (double)valid_points / (nthreads * Nshots) * 4, timing);
 
   return 0;
 }
