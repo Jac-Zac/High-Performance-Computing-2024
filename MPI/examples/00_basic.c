29d13f1b61028adbfb5b15b7c339147619d70b26
Jac-Zac <jacopozac@icloud.com>
2025-08-09 10:10:54 +0200

    updated MPI

--- MPI/examples/00_basic.c
+++ MPI/examples/00_basic.c
@@ -1,32 +1,28 @@
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <mpi.h>
 
+int main(int argc, char **argv) {
 
-int main ( int argc, char **argv )
-{
-
-  printf ("--> argc is %d\n", argc );
+  printf("--> argc is %d\n", argc);
   int Rank;
   int Ntasks;
 
-  MPI_Init ( &argc, &argv );
+  MPI_Init(&argc, &argv);
 
-  MPI_Comm_rank( MPI_COMM_WORLD, &Rank );
-  MPI_Comm_size( MPI_COMM_WORLD, &Ntasks );
+  MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &Ntasks);
 
   int planets;
   int myplanets;
-  
-  planets = (argc > 1? atoi(*(argv+1)) : 0 );
+
+  planets = (argc > 1 ? atoi(*(argv + 1)) : 0);
 
   myplanets = planets / Ntasks;
-  
-  printf( "Hello world, I'm task %d among %d friends, i'll model %d planets\n",
-	  Rank, Ntasks, myplanets );
-  
+
+  printf("Hello world, I'm task %d among %d friends, i'll model %d planets\n",
+         Rank, Ntasks, myplanets);
 
   MPI_Finalize();
 
