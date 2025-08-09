eafca0022f3805314bfd2976c341d8b665333639
Jac-Zac <jacopozac@icloud.com>
2025-08-09 10:47:36 +0200

    review mpi

--- MPI/examples/01_send_and_recv.deadlock.c
+++ MPI/examples/01_send_and_recv.deadlock.c
@@ -1,81 +1,77 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-
-int main ( int argc, char **argv )
-{
+int main(int argc, char **argv) {
 
   int Rank;
   int Ntasks;
 
-  MPI_Init ( &argc, &argv );
+  MPI_Init(&argc, &argv);
 
-  MPI_Comm_rank( MPI_COMM_WORLD, &Rank );
-  MPI_Comm_size( MPI_COMM_WORLD, &Ntasks );
+  MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &Ntasks);
 
   char hostname[100];
   int hostname_size = 100;
-  gethostname( &hostname[0], hostname_size );
+  gethostname(&hostname[0], hostname_size);
 
-  printf( "Rank %d is running on host %s\n", Rank, hostname );
+  printf("Rank %d is running on host %s\n", Rank, hostname);
 
   MPI_Barrier(MPI_COMM_WORLD);
-  
+
   int Im_even = (Rank % 2 == 0);
   int my_buddy;
-  if ( Im_even )
-    my_buddy = Rank+1;
+  if (Im_even)
+    my_buddy = Rank + 1;
   else
-    my_buddy = Rank-1;
+    my_buddy = Rank - 1;
 
-  if ( my_buddy < Ntasks )
-    {
+  if (my_buddy < Ntasks) {
 
-      #define FIRST_ROUND 0
-      #define SECOND_ROUND 1
+#define FIRST_ROUND 0
+#define SECOND_ROUND 1
 
-      /*
-       * here it is guaranteed that we end in a deadlock, because the MPI_Recv is
-       * _blocking_ in the sense that it will never return before the recection
-       * of the message has ended.
-       */
+    /*
+     * here it is guaranteed that we end in a deadlock, because the MPI_Recv is
+     * _blocking_ in the sense that it will never return before the recection
+     * of the message has ended.
+     */
 
-      
-      if ( Im_even )
-	{
-	  int        buddy_rank;
-	  MPI_Status status;
+    if (Im_even) {
+      int buddy_rank;
+      MPI_Status status;
 
-	  MPI_Recv( &buddy_rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD, &status );
-	  MPI_Send( &Rank, 1, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD );	  
+      MPI_Recv(&buddy_rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD,
+               &status);
+      MPI_Send(&Rank, 1, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD);
 
-	  if ( buddy_rank == my_buddy )
-	    printf("\tRank %d says: I confirm I have received a reply from my buddy\n", Rank);
-	  else
-	    printf("oops, a stranger has replied to me\n");	  
-	}
+      if (buddy_rank == my_buddy)
+        printf(
+            "\tRank %d says: I confirm I have received a reply from my buddy\n",
+            Rank);
       else
-	{
-	  int        buddy_rank;
-	  MPI_Status status;
+        printf("oops, a stranger has replied to me\n");
+    } else {
+      int buddy_rank;
+      MPI_Status status;
 
-	  MPI_Recv( &buddy_rank, 1, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD, &status );
-	  MPI_Send( &Rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD );
-	  
+      MPI_Recv(&buddy_rank, 1, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD,
+               &status);
+      MPI_Send(&Rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD);
 
-	  if ( buddy_rank == my_buddy )
-	    printf("Rank %d says: I confirm I have received the message from my buddy\n", Rank);
-	  else
-	    printf("oops, a stranger has communicated with me\n");	  
-	}
-      
+      if (buddy_rank == my_buddy)
+        printf("Rank %d says: I confirm I have received the message from my "
+               "buddy\n",
+               Rank);
+      else
+        printf("oops, a stranger has communicated with me\n");
     }
-  else
-    printf("Rank %d has got nobody to talk with\n", Rank );
-    
+
+  } else
+    printf("Rank %d has got nobody to talk with\n", Rank);
 
   MPI_Finalize();
 
--- MPI/examples/02_send_and_recv.may_deadlock.c
+++ MPI/examples/02_send_and_recv.may_deadlock.c
@@ -1,142 +1,139 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+#define HNAME_LEN 100
 
-
- #define HNAME_LEN 100
-
-void get_hostname( char *name )
-{
+void get_hostname(char *name) {
   int hostname_len = HNAME_LEN;
-  gethostname( name, hostname_len );
+  gethostname(name, hostname_len);
   return;
 }
 
-  
-
-int main ( int argc, char **argv )
-{
+int main(int argc, char **argv) {
 
   char hostname[HNAME_LEN];
-  get_hostname( hostname );
-  
-  int  Rank;
-  int  Ntasks;
-  int  MsgSize;
-  
-  MPI_Init ( &argc, &argv );
-  MPI_Comm_rank( MPI_COMM_WORLD, &Rank );
-  MPI_Comm_size( MPI_COMM_WORLD, &Ntasks );
+  get_hostname(hostname);
 
-  MsgSize = (argc > 1 ? atoi(*(argv+1)) : 1 );    
-  
+  int Rank;
+  int Ntasks;
+  int MsgSize;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &Ntasks);
+
+  MsgSize = (argc > 1 ? atoi(*(argv + 1)) : 1);
+
   int Im_even = (Rank % 2 == 0);
   int my_buddy;
-  if ( Im_even )
-    my_buddy = Rank+1;
+  if (Im_even)
+    my_buddy = Rank + 1;
   else
-    my_buddy = Rank-1;
+    my_buddy = Rank - 1;
 
   printf("Rank %d has buddy %d, message size is %d\n", Rank, my_buddy, MsgSize);
 
   MPI_Barrier(MPI_COMM_WORLD);
-  
-  if ( my_buddy < Ntasks )
-    {
 
-      #define FIRST_ROUND 0
-      #define SECOND_ROUND 1
+  if (my_buddy < Ntasks) {
+
+#define FIRST_ROUND 0
+#define SECOND_ROUND 1
+
+    /*
+     * here it is highly possible (not guaranteed) that we end in a deadlock,
+     * because the MPI_Send are posted concurrently.
+     * The MPI specifications leave to the implementer to decide whether
+     * MPI_Send is _blocking_ in the sense that it will never return before the
+     * reception signal has been posted OR before the availability of the
+     * local sent buffer.
+     * The point is that the beheviour may depends on the SIZE of the message.
+     * For short-enough messages (i.e. few KB), the data are sent immediately
+     * and some caching happens at receiver-size if the corresponding Recv
+     * (i.e. the dedicated memory space has not been guaranteed) has not been
+     * posted yet.
+     * That is called "eager" protocal.
+     * For larger messages, a "rendezvous" protocal may be adopted: only the
+     * requests for sending are posted, and the actual send happens when the
+     * corresponding Recv is issued.
+     * NOTE: what is "small" and  "large" is implementation dependent.
+     *
+     */
+
+    if (Im_even) {
+      /*
+       * ------------------------------------------------
+       *  send the message
+       */
+      int *message = (int *)malloc(MsgSize * sizeof(int));
+      for (int j = 0; j < MsgSize; j++)
+        message[j] = Rank;
+
+      MPI_Send((void *)message, MsgSize, MPI_INT, my_buddy, FIRST_ROUND,
+               MPI_COMM_WORLD);
 
       /*
-       * here it is highly possible (not guaranteed) that we end in a deadlock,
-       * because the MPI_Send are posted concurrently.
-       * The MPI specifications leave to the implementer to decide whether
-       * MPI_Send is _blocking_ in the sense that it will never return before the
-       * reception signal has been posted OR before the availability of the
-       * local sent buffer.
-       * The point is that the beheviour may depends on the SIZE of the message.
-       * For short-enough messages (i.e. few KB), the data are sent immediately
-       * and some caching happens at receiver-size if the corresponding Recv
-       * (i.e. the dedicated memory space has not been guaranteed) has not been
-       * posted yet.
-       * That is called "eager" protocal.
-       * For larger messages, a "rendezvous" protocal may be adopted: only the
-       * requests for sending are posted, and the actual send happens when the
-       * corresponding Recv is issued.
-       * NOTE: what is "small" and  "large" is implementation dependent.
-       *
+       * ------------------------------------------------
+       *  get the answer
        */
 
-      
-      if ( Im_even )
-	{
-	  /*
-	   * ------------------------------------------------
-	   *  send the message
-	   */
-	  int       *message = (int*)malloc( MsgSize * sizeof(int) );
-	  for( int  j = 0; j < MsgSize; j++ )
-	    message[j] = Rank;
-	  
-	  MPI_Send( (void*)message, MsgSize, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD );	  
+      int buddy_rank;
+      MPI_Status status;
 
-	  /*
-	   * ------------------------------------------------
-	   *  get the answer
-	   */
-	  
-	  int        buddy_rank;
-	  MPI_Status status;
+      MPI_Recv((void *)&buddy_rank, 1, MPI_INT, my_buddy, SECOND_ROUND,
+               MPI_COMM_WORLD, &status);
 
-	  MPI_Recv( (void*)&buddy_rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD, &status );
-
-	  if ( buddy_rank == my_buddy )
-	    printf("[R] Rank %d says: I confirm I have received a reply from my buddy\n", Rank);
-	  else
-	    printf("\t[R] Rank %d says: oops, a stranger (id: %d, expected: %d) has replied to me\n",
-		   Rank, buddy_rank, my_buddy);
-
-	  free(message);
-	}
+      if (buddy_rank == my_buddy)
+        printf("[R] Rank %d says: I confirm I have received a reply from my "
+               "buddy\n",
+               Rank);
       else
-	{
+        printf("\t[R] Rank %d says: oops, a stranger (id: %d, expected: %d) "
+               "has replied to me\n",
+               Rank, buddy_rank, my_buddy);
 
-	  /*
-	   * ------------------------------------------------
-	   *  send the answer first
-	   *  that may NOT result in a deadlock; however, it
-	   *  is NOT guaranteed that on every system/implementation
-	   *  you get through seamlessly
-	   *
-	   */
-	  MPI_Send( (void*)&Rank, 1, MPI_INT, my_buddy, SECOND_ROUND, MPI_COMM_WORLD );	  
-	  
-	  /*
-	   * ------------------------------------------------
-	   *  get the message
-	   */
-	  
-	  MPI_Status status;	  
-	  int       *message = (int*)malloc( MsgSize * sizeof(int) );
-	  
-	  MPI_Recv( (void*)message, MsgSize, MPI_INT, my_buddy, FIRST_ROUND, MPI_COMM_WORLD, &status );
-	  
-	  if ( message[0] == my_buddy )
-	    printf("Rank %d says: I confirm I have received the message from my buddy\n", Rank);
-	  else
-	    printf("Rank %d says: oops, a stranger (id: %d, expected: %d) has communicated with me\n",
-		   Rank, message[0], my_buddy);
+      free(message);
+    } else {
 
-	  free(message);
-	}
-      
+      /*
+       * ------------------------------------------------
+       *  send the answer first
+       *  that may NOT result in a deadlock; however, it
+       *  is NOT guaranteed that on every system/implementation
+       *  you get through seamlessly
+       *
+       */
+      MPI_Send((void *)&Rank, 1, MPI_INT, my_buddy, SECOND_ROUND,
+               MPI_COMM_WORLD);
+
+      /*
+       * ------------------------------------------------
+       *  get the message
+       */
+
+      MPI_Status status;
+      int *message = (int *)malloc(MsgSize * sizeof(int));
+
+      MPI_Recv((void *)message, MsgSize, MPI_INT, my_buddy, FIRST_ROUND,
+               MPI_COMM_WORLD, &status);
+
+      if (message[0] == my_buddy)
+        printf("Rank %d says: I confirm I have received the message from my "
+               "buddy\n",
+               Rank);
+      else
+        printf("Rank %d says: oops, a stranger (id: %d, expected: %d) has "
+               "communicated with me\n",
+               Rank, message[0], my_buddy);
+
+      free(message);
     }
-  else
-    printf("Rank %d has got nobody to talk with\n", Rank );
-    
+
+  } else
+    printf("Rank %d has got nobody to talk with\n", Rank);
 
   MPI_Finalize();
 
